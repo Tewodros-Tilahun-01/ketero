35b906cf51f0b83b68fdeff010392de604ae24d2
teddy <teddytilahun02@gmail.com>
2024-10-13 21:42:36 +0300

    editing port

--- server/app.js
+++ server/app.js
@@ -44,6 +44,6 @@ app.use(passport.session());
 
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
