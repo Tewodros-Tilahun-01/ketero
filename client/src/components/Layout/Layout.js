b64d39d26daeb975918626aab07c24fc99c052fd
teddy <teddytilahun02@gmail.com>
2024-10-21 21:38:50 +0300

    adding if authenticated redirect it to the dashboard logic

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -2,12 +2,48 @@ import React from "react";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
 import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import LoadingPage from "../loadingPage/LoadingPage";
+
 function Layout() {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const isAuth = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/isauth", {
+          withCredentials: true, // Enable sending cookies with the request
+        });
+
+        if (response.data.userAuthenticated) {
+          navigate(`/${response.data.role}dashboard`);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    isAuth();
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 8000); // 7 seconds loading
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
   return (
     <>
-      <Header />
-      <Outlet /> {/* This will render the matched child route components */}
-      <Footer />
+      {loading ? (
+        <LoadingPage />
+      ) : (
+        <>
+          <Header />
+          <Outlet /> {/* This will render the matched child route components */}
+          <Footer />
+        </>
+      )}
     </>
   );
 }
