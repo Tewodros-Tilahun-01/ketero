e444749cf420970d17da4f9e6b886d71bde0cfc2
teddy <teddytilahun02@gmail.com>
2024-10-25 15:57:29 +0300

    debug

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000", // Specify your frontend domain
+    origin: "https://localhost:3000", // Specify your frontend domain
     credentials: true, // Enable sending cookies from the browser
   })
 );
