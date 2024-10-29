54a518474f1acce23122a01cccd87f7d2ebf4513
teddy <teddytilahun02@gmail.com>
2024-10-29 22:52:33 +0300

    integrating email

---
+++ server/config/mail.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+const sendEmail = (to, subject, text) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: to,
+    subject: subject,
+    text: text,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+module.exports = sendEmail;
